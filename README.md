4df4b1e9da232e60089798cadd275386e2c87cfa
cavalleria <605370459@qq.com>
2020-05-20 04:32:29 -0700

    update

--- README.md
+++ README.md
@@ -34,7 +34,7 @@
 ## Requirements
 * Python >= 3.5 (3.6 recommended)
 * PyTorch >= 0.4 (1.2 recommended)
-* tqdm (Optional for `test.py`)
+* tqdm
 * tensorboard >= 1.14 (see [Tensorboard Visualization](#tensorboard-visualization))
 
 ## Features
@@ -51,44 +51,34 @@
   ```
   humanseg.pytorch/
   │
-  ├── train.py - main script to start training
-  ├── test.py - evaluation of trained model
+  ├── base
+  │   ├── base_data_loader.py
+  │   ├── base_inference.py
+  │   ├── base_model.py
+  │   ├── base_trainer.py
+  │   └── __init__.py
+  ├── config
   │
-  ├── config.json - holds configuration for training
-  ├── parse_config.py - class to handle config file and cli options
-  │
-  ├── new_project.py - initialize new project with template files
-  │
-  ├── base/ - abstract base classes
-  │   ├── base_data_loader.py
-  │   ├── base_model.py
-  │   └── base_trainer.py
-  │
-  ├── data_loader/ - anything about data loading goes here
-  │   └── data_loaders.py
-  │
-  ├── data/ - default directory for storing input data
-  │
-  ├── model/ - models, losses, and metrics
-  │   ├── model.py
-  │   ├── metric.py
-  │   └── loss.py
-  │
-  ├── saved/
-  │   ├── models/ - trained models are saved here
-  │   └── log/ - default logdir for tensorboard and logging output
-  │
-  ├── trainer/ - trainers
-  │   └── trainer.py
-  │
-  ├── logger/ - module for tensorboard visualization and logging
-  │   ├── visualization.py
-  │   ├── logger.py
-  │   └── logger_config.json
-  │  
-  └── utils/ - small utility functions
-      ├── util.py
-      └── ...
+  ├── data_loader
+  │   ├── data_loaders.py
+  │   └── transforms.py
+  ├── LICENSE
+  ├── logger
+  │   ├── __init__.py
+  │   ├── logger_config.json
+  │   ├── logger.py
+  │   └── visualization.py
+  ├── README.md
+  ├── test.py
+  ├── trainer
+  │   ├── __init__.py
+  │   └── trainer.py
+  ├── train.py
+  └── utils
+      ├── __init__.py
+      ├── loss.py
+      ├── metric.py
+      └── util.py
   ```
 
 ## Usage
@@ -99,55 +89,92 @@ Try `python train.py -c config.json` to run code.
 Config files are in `.json` format:
 ```javascript
 {
-  "name": "Mnist_LeNet",        // training session name
-  "n_gpu": 1,                   // number of GPUs to use for training.
-  
-  "arch": {
-    "type": "MnistModel",       // name of model architecture to train
-    "args": {
+    "name": "HumanSeg",
+    "n_gpu": 1,
+    
+    "arch": {
+        "type": "UNet",
+        "args": {
+            "backbone": "mobilenetv2",
+            "num_classes": 2,
+            "pretrained_backbone": null
+        }
+    },
 
-    }                
-  },
-  "data_loader": {
-    "type": "MnistDataLoader",         // selecting data loader
-    "args":{
-      "data_dir": "data/",             // dataset path
-      "batch_size": 64,                // batch size
-      "shuffle": true,                 // shuffle training data before splitting
-      "validation_split": 0.1          // size of validation dataset. float(portion) or int(number of samples)
-      "num_workers": 2,                // number of cpu processes to be used for data loading
-    }
-  },
-  "optimizer": {
-    "type": "Adam",
-    "args":{
-      "lr": 0.001,                     // learning rate
-      "weight_decay": 0,               // (optional) weight decay
-      "amsgrad": true
-    }
-  },
-  "loss": "nll_loss",                  // loss
-  "metrics": [
-    "accuracy", "top_k_acc"            // list of metrics to evaluate
-  ],                         
-  "lr_scheduler": {
-    "type": "StepLR",                  // learning rate scheduler
-    "args":{
-      "step_size": 50,          
-      "gamma": 0.1
-    }
-  },
-  "trainer": {
-    "epochs": 100,                     // number of training epochs
-    "save_dir": "saved/",              // checkpoints are saved in save_dir/models/name
-    "save_freq": 1,                    // save checkpoints every save_freq epochs
-    "verbosity": 2,                    // 0: quiet, 1: per epoch, 2: full
-  
-    "monitor": "min val_loss"          // mode and metric for model performance monitoring. set 'off' to disable.
-    "early_stop": 10	                 // number of epochs to wait before early stop. set 0 to disable.
-  
-    "tensorboard": true,               // enable tensorboard visualization
-  }
+	"train_loader": {
+		"type": "SegmentationDataLoader",
+		"args":{
+			"pairs_file": "dataset/train_mask.txt",
+			"color_channel": "RGB",
+			"resize": 320,
+			"padding_value": 0,
+			"is_training": true,
+			"noise_std": 3,
+			"crop_range": [0.90, 1.0],
+			"flip_hor": 0.5,
+			"rotate": 0.0,
+			"angle": 10,
+			"normalize": true,
+			"one_hot": false,
+			"shuffle": true,
+			"batch_size": 16,
+			"n_workers": 24,
+			"pin_memory": true
+		}
+	},
+
+	"valid_loader": {
+		"type": "SegmentationDataLoader",
+		"args":{
+			"pairs_file": "dataset/valid_mask.txt",
+			"color_channel": "RGB",
+			"resize": 320,
+			"padding_value": 0,
+			"is_training": false,
+			"normalize": true,
+			"one_hot": false,
+			"shuffle": false,
+			"batch_size": 16,
+			"n_workers": 24,
+			"pin_memory": true
+		}
+	},
+
+	"optimizer": {
+		"type": "SGD",
+		"args":{
+			"lr": 1e-2,
+			"momentum": 0.9,
+			"weight_decay": 1e-8
+		}
+	},
+
+	"loss": "dice_loss",
+	"metrics": [
+		"miou"
+	],
+
+	"lr_scheduler": {
+		"type":"StepLR",
+		"args":{
+			"step_size": 100,
+			"gamma": 1.0
+		}
+	},
+
+	"trainer": {
+		"epochs": 80,
+		"save_dir": "/workspace/checkpoints/",
+		"save_freq": null,
+		"verbosity": 2,
+		"monitor": "valid_loss",
+		"monitor_mode": "min"
+	},
+
+	"visualization":{
+		"tensorboardX": true,
+		"log_dir": "/workspace/checkpoints/"
+	}
 }
 ```
 
@@ -180,11 +207,6 @@ Specify indices of available GPUs by cuda environmental variable.
   ```
 
 ## Customization
-
-### Project initialization
-Use the `new_project.py` script to make your new project directory with template files.
-`python new_project.py ../NewProject` then a new project folder named 'NewProject' will be made.
-This script will filter out unneccessary files like cache, git files or readme file. 
 
 ### Custom CLI options
 
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,109 +1,133 @@
-import numpy as np
 import torch
+import numpy as np
+from tqdm import tqdm
+import torch.nn.functional as F
 from torchvision.utils import make_grid
-from base import BaseTrainer
-from utils import inf_loop, MetricTracker
+from base.base_trainer import BaseTrainer
 
+def poly_lr_scheduler(optimizer, init_lr, curr_iter, max_iter, power=0.9):
+	for g in optimizer.param_groups:
+		g['lr'] = init_lr * (1 - curr_iter/max_iter)**power
 
 class Trainer(BaseTrainer):
-    """
-    Trainer class
-    """
-    def __init__(self, model, criterion, metric_ftns, optimizer, config, data_loader,
-                 valid_data_loader=None, lr_scheduler=None, len_epoch=None):
-        super().__init__(model, criterion, metric_ftns, optimizer, config)
-        self.config = config
-        self.data_loader = data_loader
-        if len_epoch is None:
-            # epoch-based training
-            self.len_epoch = len(self.data_loader)
-        else:
-            # iteration-based training
-            self.data_loader = inf_loop(data_loader)
-            self.len_epoch = len_epoch
-        self.valid_data_loader = valid_data_loader
-        self.do_validation = self.valid_data_loader is not None
-        self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
+	def __init__(self, model, loss, metrics, optimizer, resume, config,
+				 data_loader, valid_data_loader=None, lr_scheduler=None, train_logger=None):
+		super(Trainer, self).__init__(model, loss, metrics, optimizer, resume, config, train_logger)
+		self.config = config
+		self.data_loader = data_loader
+		self.valid_data_loader = valid_data_loader
+		self.do_validation = self.valid_data_loader is not None
+		self.lr_scheduler = lr_scheduler
+		self.max_iter = len(self.data_loader) * self.epochs
+		self.init_lr = optimizer.param_groups[0]['lr']
 
-        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
-        self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
 
-    def _train_epoch(self, epoch):
-        """
-        Training logic for an epoch
+	def _eval_metrics(self, output, target):
+		acc_metrics = np.zeros(len(self.metrics))
+		for i, metric in enumerate(self.metrics):
+			acc_metrics[i] += metric(output, target)
+		return acc_metrics
 
-        :param epoch: Integer, current training epoch.
-        :return: A log that contains average loss and metric in this epoch.
-        """
-        self.model.train()
-        self.train_metrics.reset()
-        for batch_idx, (data, target) in enumerate(self.data_loader):
-            data, target = data.to(self.device), target.to(self.device)
 
-            self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.criterion(output, target)
-            loss.backward()
-            self.optimizer.step()
+	def _train_epoch(self, epoch):
+		print("Train on epoch...")
+		self.model.train()
+		self.writer_train.set_step(epoch)
+	
+		# Perform training
+		total_loss = 0
+		total_metrics = np.zeros(len(self.metrics))
+		n_iter = len(self.data_loader)
+		for batch_idx, (data, target) in tqdm(enumerate(self.data_loader), total=n_iter):
+			curr_iter = batch_idx + (epoch-1)*n_iter
+			data, target = data.to(self.device), target.to(self.device)
+			self.optimizer.zero_grad()
+			output = self.model(data)
+			loss = self.loss(output, target)
+			loss.backward()
+			self.optimizer.step()
 
-            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
-            self.train_metrics.update('loss', loss.item())
-            for met in self.metric_ftns:
-                self.train_metrics.update(met.__name__, met(output, target))
+			total_loss += loss.item()
+			total_metrics += self._eval_metrics(output, target)
 
-            if batch_idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
-                    epoch,
-                    self._progress(batch_idx),
-                    loss.item()))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+			if (batch_idx==n_iter-2) and (self.verbosity>=2):
+				self.writer_train.add_image('train/input', make_grid(data[:,:3,:,:].cpu(), nrow=4, normalize=True))
+				self.writer_train.add_image('train/label', make_grid(target.unsqueeze(1).cpu(), nrow=4, normalize=True))
+				if type(output)==tuple or type(output)==list:
+					self.writer_train.add_image('train/output', make_grid(F.softmax(output[0], dim=1)[:,1:2,:,:].cpu(), nrow=4, normalize=True))
+				else:
+					# self.writer_train.add_image('train/output', make_grid(output.cpu(), nrow=4, normalize=True))
+					self.writer_train.add_image('train/output', make_grid(F.softmax(output, dim=1)[:,1:2,:,:].cpu(), nrow=4, normalize=True))
 
-            if batch_idx == self.len_epoch:
-                break
-        log = self.train_metrics.result()
+			poly_lr_scheduler(self.optimizer, self.init_lr, curr_iter, self.max_iter, power=0.9)
 
-        if self.do_validation:
-            val_log = self._valid_epoch(epoch)
-            log.update(**{'val_'+k : v for k, v in val_log.items()})
+		# Record log
+		total_loss /= len(self.data_loader)
+		total_metrics /= len(self.data_loader)
+		log = {
+			'train_loss': total_loss,
+			'train_metrics': total_metrics.tolist(),
+		}
 
-        if self.lr_scheduler is not None:
-            self.lr_scheduler.step()
-        return log
+		# Write training result to TensorboardX
+		self.writer_train.add_scalar('loss', total_loss)
+		for i, metric in enumerate(self.metrics):
+			self.writer_train.add_scalar('metrics/%s'%(metric.__name__), total_metrics[i])
 
-    def _valid_epoch(self, epoch):
-        """
-        Validate after training an epoch
+		if self.verbosity>=2:
+			for i in range(len(self.optimizer.param_groups)):
+				self.writer_train.add_scalar('lr/group%d'%(i), self.optimizer.param_groups[i]['lr'])
 
-        :param epoch: Integer, current training epoch.
-        :return: A log that contains information about validation
-        """
-        self.model.eval()
-        self.valid_metrics.reset()
-        with torch.no_grad():
-            for batch_idx, (data, target) in enumerate(self.valid_data_loader):
-                data, target = data.to(self.device), target.to(self.device)
+		# Perform validating
+		if self.do_validation:
+			print("Validate on epoch...")
+			val_log = self._valid_epoch(epoch)
+			log = {**log, **val_log}
 
-                output = self.model(data)
-                loss = self.criterion(output, target)
+		# Learning rate scheduler
+		if self.lr_scheduler is not None:
+			self.lr_scheduler.step()
 
-                self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
-                self.valid_metrics.update('loss', loss.item())
-                for met in self.metric_ftns:
-                    self.valid_metrics.update(met.__name__, met(output, target))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+		return log
 
-        # add histogram of model parameters to the tensorboard
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins='auto')
-        return self.valid_metrics.result()
 
-    def _progress(self, batch_idx):
-        base = '[{}/{} ({:.0f}%)]'
-        if hasattr(self.data_loader, 'n_samples'):
-            current = batch_idx * self.data_loader.batch_size
-            total = self.data_loader.n_samples
-        else:
-            current = batch_idx
-            total = self.len_epoch
-        return base.format(current, total, 100.0 * current / total)
+	def _valid_epoch(self, epoch):
+		self.model.eval()
+		total_val_loss = 0
+		total_val_metrics = np.zeros(len(self.metrics))
+		n_iter = len(self.valid_data_loader)
+		self.writer_valid.set_step(epoch)
+
+		with torch.no_grad():
+			# Validate
+			for batch_idx, (data, target) in tqdm(enumerate(self.valid_data_loader), total=n_iter):
+				data, target = data.to(self.device), target.to(self.device)
+				output = self.model(data)
+				loss = self.loss(output, target)
+
+				total_val_loss += loss.item()
+				total_val_metrics += self._eval_metrics(output, target)
+
+				if (batch_idx==n_iter-2) and(self.verbosity>=2):
+					self.writer_valid.add_image('valid/input', make_grid(data[:,:3,:,:].cpu(), nrow=4, normalize=True))
+					self.writer_valid.add_image('valid/label', make_grid(target.unsqueeze(1).cpu(), nrow=4, normalize=True))
+					if type(output)==tuple or type(output)==list:
+						self.writer_valid.add_image('valid/output', make_grid(F.softmax(output[0], dim=1)[:,1:2,:,:].cpu(), nrow=4, normalize=True))
+					else:
+						# self.writer_valid.add_image('valid/output', make_grid(output.cpu(), nrow=4, normalize=True))
+						self.writer_valid.add_image('valid/output', make_grid(F.softmax(output, dim=1)[:,1:2,:,:].cpu(), nrow=4, normalize=True))
+
+			# Record log
+			total_val_loss /= len(self.valid_data_loader)
+			total_val_metrics /= len(self.valid_data_loader)
+			val_log = {
+				'valid_loss': total_val_loss,
+				'valid_metrics': total_val_metrics.tolist(),
+			}
+
+			# Write validating result to TensorboardX
+			self.writer_valid.add_scalar('loss', total_val_loss)
+			for i, metric in enumerate(self.metrics):
+				self.writer_valid.add_scalar('metrics/%s'%(metric.__name__), total_val_metrics[i])
+
+		return val_log
