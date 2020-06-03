546533b5d94b391dc020dc9d170e42550ed3af11
cavalleria <605370459@qq.com>
2020-06-02 22:28:09 -0700

    update

--- tools/video_infer.sh
+++ tools/video_infer.sh
@@ -1,7 +1,7 @@
 #--bg ./tools/bg2.jpg \
 #--net xception65 \
 
-CUDA_VISIBLE_DEVICES="3" python tools/video_infer.py \
+CUDA_VISIBLE_DEVICES="3" python tools/videoinfer.py \
     --use_cuda \
     --model hrnet \
     --checkpoint ./ckpts/hrnet_w18_small_v2.pth \
